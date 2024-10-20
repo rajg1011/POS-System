18bd68294c3b312c5ff347e520b57ce0004a18d8
RAJ <raj2356.be21@chitkara.edu.in>
2024-10-21 00:46:00 +0530

    More Addition

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-import Items from "./items";
+import Items from "./items.js";
 
+console.log(Items)
 const itemsGrid = document.querySelector(".items-grid");
+const sectionVarButtons= Array.from(document.querySelectorAll(".section-types"));
+console.log(sectionVarButtons)
 
-window.addEventListener("load", () => {
-  Items.vegetables.forEach((item) => {
-    const div = document.createElement("div");
-    div.className = "item";
-    div.textContent = item.name;
-    itemsGrid.childNodes = div;
-  });
+const showItems= (Type)=>{
+    Type.forEach((item) => {
+        const div = document.createElement("div");
+        div.className = "item";
+        div.textContent = item.name;
+        itemsGrid.appendChild(div);
+      });
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    showItems(Items.vegetables);
 });
+
+sectionVarButtons.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        const dataType=btn.getAttribute("data-type");
+        if(dataType!=='extra'){
+            return showItems(Items.dataType);
+        }
+    })
+})
